5a669a7d55a82418a187abcd6ee0f5df0c854bfe
jaisachin <138647690+SAZZ-007@users.noreply.github.com>
2023-08-17 00:34:53 +0530

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,8 +1,17 @@
 # 🚨 Don't change the code below 👇
 height = float(input("enter your height in m: "))
-weight = int(input("enter your weight in kg: "))
+weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-BMI= weight/(height*height)
-print(int(BMI))
+BMI = weight/height**2
+if BMI < 18.5:
+   print("they are underweight")
+elif BMI < 25:
+   print("it is normal weight")
+elif BMI < 30:
+  print("overweight")
+elif BMI < 35: 
+  print("obese")
+else:
+  print("clincially obese")     
